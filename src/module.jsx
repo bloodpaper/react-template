065171e3615b9ae5afbb457642a5a06bd16a269c
bloodpaper <bloodpaper@list.ru>
2022-07-31 22:23:34 +0400

    init prodject

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import andrey from './img/998.jpg';
+
+const Module = () => (
+	<>
+		<h1>Hello, React</h1>
+		<img width={333} src={andrey} />
+	</>
+);
+
+export default Module;
